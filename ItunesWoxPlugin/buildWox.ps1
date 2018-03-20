21145786e78e78a42d98a47df6c12bd1698dfeac
Satya Prakash <satya.prakash@itron.com>
2018-03-20 14:27:02 +0530

    changing output folder for wox file

--- ItunesWoxPlugin/buildWox.ps1
+++ ItunesWoxPlugin/buildWox.ps1
@@ -1,5 +1,5 @@
-$source = "C:\Projects\WoxPlugin\PluginOutput\LooseFile\"
-$destination = "C:\Projects\WoxPlugin\PluginOutput\ItunesBuild.wox"
+$source = "C:\Users\sprakash\Documents\Visual Studio 2017\Projects\ItunesWoxPlug\ItunesWoxPlugin\bin\Debug"
+$destination = "C:\Users\sprakash\Documents\Visual Studio 2017\Projects\ItunesWoxPlug\ItunesWoxPlugin\bin\ItunesBuild.wox"
 
  If(Test-path $destination) {Remove-item $destination}
 Add-Type -assembly "system.io.compression.filesystem"
